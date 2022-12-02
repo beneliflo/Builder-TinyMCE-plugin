7b9497bdfefb31530386f8aca96414f318242c96
Benjamin Flores <beneliflo@gmail.com>
2022-12-02 14:55:51 -0300

    update toolbar

--- src/plugin.jsx
+++ src/plugin.jsx
@@ -47,9 +47,9 @@ function TinyEditor(props) {
           ],
           toolbar:
             'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter | code | ' +
+            'bold italic forecolor | alignleft aligncenter alignright alignjustify | code | ' +
             'link image media | table mergetags | ' +
-            'alignright alignjustify | checklist bullist numlist outdent indent | ' +
+            'checklist bullist numlist outdent indent | ' +
             'removeformat | help',
           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
         }}
