0bea3324cb979f796a8fe923e121416ca94c78ca
Benjamin Flores <beneliflo@gmail.com>
2023-01-03 19:18:56 -0300

    remove image uploader handler

--- src/plugin.jsx
+++ src/plugin.jsx
@@ -8,46 +8,6 @@ import { Builder } from '@builder.io/react';
 import React, { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
-const example_image_upload_handler = (blobInfo, progress) => new Promise((resolve, reject) => {
-  const xhr = new XMLHttpRequest();
-  xhr.withCredentials = false;
-  xhr.open('POST', 'postAcceptor.php');
-
-  xhr.upload.onprogress = (e) => {
-    progress(e.loaded / e.total * 100);
-  };
-
-  xhr.onload = () => {
-    if (xhr.status === 403) {
-      reject({ message: 'HTTP Error: ' + xhr.status, remove: true });
-      return;
-    }
-
-    if (xhr.status < 200 || xhr.status >= 300) {
-      reject('HTTP Error: ' + xhr.status);
-      return;
-    }
-
-    const json = JSON.parse(xhr.responseText);
-
-    if (!json || typeof json.location != 'string') {
-      reject('Invalid JSON: ' + xhr.responseText);
-      return;
-    }
-
-    resolve(json.location);
-  };
-
-  xhr.onerror = () => {
-    reject('Image upload failed due to a XHR Transport error. Code: ' + xhr.status);
-  };
-
-  const formData = new FormData();
-  formData.append('file', blobInfo.blob(), blobInfo.filename());
-
-  xhr.send(formData);
-});
-
 function TinyEditor(props) {
   const editorRef = useRef(null);
   const log = () => {
@@ -102,15 +62,8 @@ function TinyEditor(props) {
           image_caption: true,
           image_description: false,
           images_upload_credentials: true,
-          /* enable automatic uploads of images represented by blob or data URIs*/
           automatic_uploads: true,
-          /*
-            URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
-            images_upload_url: 'postAcceptor.php',
-            here we add custom filepicker only to Image dialog
-          */
-          file_picker_types: 'file image media',
-          images_upload_handler: example_image_upload_handler
+          file_picker_types: 'file image media'
         }}
       />
       <button type="button" onClick={log}>
